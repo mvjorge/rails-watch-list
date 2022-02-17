b44dfeb09deba148e8b7a88b9d1719ce70f40f03
jorge montenegro <jmontenegrocip@gmail.com>
2022-02-17 15:53:42 -0500

    watch list

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,8 @@
+// app/javascript/controllers/navbar_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log("hello from navbar_controller!")
+  }
+}
