11ceb734de341d2705741623305c2e3151f7b768
jorge montenegro <jmontenegrocip@gmail.com>
2022-02-18 15:41:49 -0500

    localhost ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.active_storage.service = :cloudinary
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -31,7 +34,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
