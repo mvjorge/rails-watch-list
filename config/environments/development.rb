7a72b2aadf9744e40d8df6fe06f784da45124a96
jorge montenegro <jmontenegrocip@gmail.com>
2022-02-19 17:14:34 -0500

    change local to cloudinary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.active_storage.service = :cloudinary
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -35,6 +34,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
